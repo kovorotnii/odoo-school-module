5fdb4142ba1de8fa768b20959e3008581e255feb
Igor Kovorotniy <i.kovorotniy@rightech.io>
2019-09-27 17:33:10 +0300

    Updated fields

--- models/teacher.py
+++ models/teacher.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
+
+from odoo.exceptions import ValidationError
 
 class TeacherTeacher(models.Model):
     # model name
     _name = 'teacher.teacher'
 
     name = fields.Char(string='Name', required=True, default='Petr Ivanovich')
-    age = fields.Integer(string='Age', required=True)
-    teacher_dob = fields.Date(string='Date of birth')
+    age = fields.Integer(string='Age')#, compute='_check_age')
+    
+    #teacher_dob = fields.Date(string='Date of birth')
 
     student = fields.Many2one('student.student', string='Student')
+
+    # create age constrain
+   # @api.constrains('age')
+   # def _check_age(self):
+    #    for r in self:
+     #       if r.age > 70:
+       #         raise ValidationError("Your age is too high")
+   
