6386ed5b5999d71d7eee210488a9a1d257e74d49
Igor Kovorotniy <i.kovorotniy@rightech.io>
2019-09-27 15:20:48 +0300

    Added computing field func

--- models/parent.py
+++ models/parent.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
+import random
 
 class Parent(models.Model):
     # model name
@@ -8,8 +9,15 @@ class Parent(models.Model):
 
     # Names for fields set in xml
     name = fields.Char(required=True)
-    test = fields.Char()
+    test = fields.Char(compute='_compute_test')
     # link parent with student
     student = fields.Many2one('student.student') 
     # link parent with teacher
     teacher = fields.Many2one('teacher.teacher')
+
+    # while changing na,e then click next input
+    # it will generate test field value
+    @api.depends('name')
+    def _compute_test(self):
+        for record in self:
+            record.test = str(random.randint(1, 1e6))
